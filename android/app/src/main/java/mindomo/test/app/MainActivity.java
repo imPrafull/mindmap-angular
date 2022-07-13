d74f6b1df4d7c41532cef2d7bff4063f7a03fb72
imPrafull <prafullsakpal158@gmail.com>
2022-07-13 22:50:59 +0530

    added capacitor and android platform

---
+++ android/app/src/main/java/mindomo/test/app/MainActivity.java
@@ -0,0 +1,5 @@
+package mindomo.test.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mindomo.test.app',
+  appName: 'mindomo',
+  webDir: 'dist/mindomo',
+  bundledWebRuntime: false
+};
+
+export default config;
